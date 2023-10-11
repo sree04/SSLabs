a2f5f00cddff75ccd21a2d5451c93d70916e8349
sree04 <sreeparna0410@gmail.com>
2023-10-11 17:31:20 +0530

    okay

---
+++ hands on list 2/22a.c
@@ -0,0 +1,12 @@
+#include <errno.h>
+char *fifoName = "./22-fifo";
+
+void init()
+{
+    int fifoStatus;
+
+    fifoStatus = mkfifo(fifoName, S_IRWXU);
+    if (!(errno == EEXIST || fifoStatus != -1))
+        perror("Error while creating FIFO file fifoOne");
+}
+
